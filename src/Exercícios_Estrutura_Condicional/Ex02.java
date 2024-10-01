fe027beaedfa1ead0db202b4671dd05383cfb329
LeoFhin <leonardo.santos7507@gmail.com>
2024-10-01 09:11:52 -0300

    Add Ex02

---
+++ src/Exercícios_Estrutura_Condicional/Ex02.java
@@ -0,0 +1,22 @@
+package Exercícios_Estrutura_Condicional;
+
+import java.util.Scanner;
+
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe um Número: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0){
+            System.out.println("Par!");
+        }
+        else {
+            System.out.println("Impar!");
+        }
+
+    }
+}
